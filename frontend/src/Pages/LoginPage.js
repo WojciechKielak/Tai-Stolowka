9c57df1aa53bbbf0f8198524f411eb68dbad8ec9
Wojciech <wojtekkielak@gmail.com>
2023-06-07 12:58:34 +0200

    strona logowania

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -9,12 +9,36 @@ import AuthContext from '../Context/AuthContext';
 const LoginPage = () => {
     let {loginUser} = useContext(AuthContext)
     return (
-        <div>
-            <form onSubmit={loginUser}>
-                <input type="text" name="username" placeholder="Enter Username" />
-                <input type="password" name="password" placeholder="Enter Password" />
-                <input type="submit"/>
+        <div> 
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand style={{ fontSize: '30px' }}>Stołówka szkolna</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+            <div className="center">
+             <form onSubmit={loginUser}>
+                <div>
+                    <span style={{ float: 'left' ,marginTop: '50px' }}>
+                    <input type="text" name="username" placeholder="Wprowadź nazwę" />
+                    </span>                   
+                </div>
+
+                <div>
+                    <span style={{ float: 'left' ,marginTop: '20px' }}>
+                    <input type="password" name="password" placeholder="Wprowadź hasło" />
+                    </span>                
+                </div>
+                
+                <span style={{ float: 'left' ,marginTop: '20px' }}>
+                <input type="submit" value="Zaloguj się"/>
+                </span>
+                
             </form>
+            </div>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        
         </div>
     )
 }
